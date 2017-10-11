e8fc88a4aca674f6ee439b197c6066f423a7e204
StasSiya <ana.rudespen@gmail.com>
2017-10-11 16:50:30 +0200

    Add words to NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,8 +15,13 @@
  */
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +29,44 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+    //Create an ArrayList of words in the NumbersActivity
+    ArrayList <String> words = new ArrayList<String>();
+    words.add ("one");
+    words.add ("two");
+    words.add ("three");
+    words.add ("four");
+    words.add ("five");
+    words.add ("six");
+    words.add ("seven");
+    words.add ("eigth");
+    words.add ("nine");
+    words.add ("ten");
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+        /*Create a counter variable to keep track of the index position
+        int i = 0;
+        while (i<words.size()){
+            //Create a new TextView
+            TextView wordView = new TextView (this);
+            wordView.setText(words.get(i));
+            //Add the TextView as a child to the
+            rootView.addView(wordView);
+            //update counter
+            i++;
+        }*/
+
+        for (int index=0;index<words.size();index++ ){
+            TextView wordView = new TextView (this);
+
+            wordView.setText(words.get(index));
+            //Add the TextView as a child to the rootView
+            rootView.addView(wordView);
+            Log.v("NumbersActivity", "The wors is " + words.get(index));
+        }
+
+
+
+
     }
 }
