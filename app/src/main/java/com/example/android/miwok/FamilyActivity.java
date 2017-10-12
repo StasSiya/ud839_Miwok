df804aeb65673dd7d098606298e2e7a584a1cf1a
StasSiya <ana.rudespen@gmail.com>
2017-10-12 14:00:10 +0200

    Create and use one layout file for all categories "word_list.xml"

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -15,14 +15,37 @@
  */
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.word_list);
+        //Create an ArrayList of words in the NumbersActivity
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add (new Word ("one","lutti"));
+        words.add (new Word ("two","otiiko"));
+        words.add (new Word ("three","tolookosu"));
+        words.add (new Word ("four","oyyisa"));
+        words.add (new Word ("five","massokka"));
+        words.add (new Word ("six","temmokka"));
+        words.add (new Word ("seven","kenekaku"));
+        words.add (new Word ("eight","kawinta"));
+        words.add (new Word ("nine","wo'e"));
+        words.add (new Word ("ten","na'aacha"));
+
+        //Create a Wordadapter, whose data source is a the list of numbers (words)
+        //Adapter knows how to create list item views for each item in the list.
+        WordAdapter adapter = new WordAdapter (this, words);
+
+        //get a reference to the ListView and attach adapter to the ListView
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
